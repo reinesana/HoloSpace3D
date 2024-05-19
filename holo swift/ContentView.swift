dd2e909dfd07744055b42fd6f4ac609b54a7c2c1
reinesana <17shana2017@gmail.com>
2024-05-18 17:34:45 -0700

    Initial Commit

---
+++ holo swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  holo swift
+//
+//  Created by Shana Kesia Nursoo on 2024-05-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
