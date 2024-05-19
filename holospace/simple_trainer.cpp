747b45213c7df1e365dae41f206eb7faafc269a0
gpa21 <gpa21@sfu.ca>
2024-05-19 10:43:03 -0700

    update

--- holospace/simple_trainer.cpp
+++ holospace/simple_trainer.cpp
@@ -66,40 +66,23 @@ int main(int argc, char **argv){
         std::cout << "Using CPU" << std::endl;
     }
 
-    // Test image
-    // Top left red
-    // Bottom right blue
     torch::Tensor gtImage = torch::ones({height, width, 3});
     gtImage.index_put_({Slice(None, height / 2), Slice(None, width / 2), Slice()}, torch::tensor({1.0, 0.0, 0.0}));
     gtImage.index_put_({Slice(height / 2, None), Slice(width / 2, None), Slice()}, torch::tensor({0.0, 0.0, 1.0}));
 
-    // cv::Mat image = tensorToImage(gtImage);
-    // cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
-    // cv::imwrite("test.png", image);
-
     gtImage = gtImage.to(device);
-    double fovX = PI / 2.0; // horizontal field of view (90 deg)
+    double fovX = PI / 2.0;
     double focal = 0.5 * static_cast<double>(width) / std::tan(0.5 * fovX);
 
     TileBounds tileBounds = std::make_tuple((width + BLOCK_X - 1) / BLOCK_X,
                       (height + BLOCK_Y - 1) / BLOCK_Y,
                       1);
     
-    // Init gaussians
-#ifdef USE_CUDA
-    torch::cuda::manual_seed_all(0);
-#endif
-    torch::manual_seed(0);
 
-    // Random points, scales and colors
-    torch::Tensor means = 2.0 * (torch::rand({numPoints, 3}, torch::kCPU) - 0.5); // Positions [-1, 1]
+    torch::manual_seed(0);
+    torch::Tensor means = 2.0 * (torch::rand({numPoints, 3}, torch::kCPU) - 0.5); 
     torch::Tensor scales = torch::rand({numPoints, 3}, torch::kCPU);
-    // torch::Tensor means = torch::tensor({{0.5f, 0.5f, -5.0f}, {0.5f, 0.5f, -6.0f}, {0.25f, 0.25f, -4.0f}}, torch::kCPU);
-    // torch::Tensor scales = torch::tensor({{0.5f, 0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}, {1.0f, 1.0f, 1.0f}}, torch::kCPU);
     torch::Tensor rgbs = torch::rand({numPoints, 3}, torch::kCPU);
-    
-    // Random rotations (quaternions)
-    // quats = ( sqrt(1-u) sin(2πv), sqrt(1-u) cos(2πv), sqrt(u) sin(2πw), sqrt(u) cos(2πw))
     torch::Tensor u = torch::rand({numPoints, 1}, torch::kCPU);
     torch::Tensor v = torch::rand({numPoints, 1}, torch::kCPU);
     torch::Tensor w = torch::rand({numPoints, 1}, torch::kCPU);
@@ -119,8 +102,6 @@ int main(int argc, char **argv){
             }, -1);
     
     torch::Tensor opacities = torch::ones({numPoints, 1}, device);
-
-    // View matrix (translation in Z by 8 units)
     torch::Tensor viewMat = torch::tensor({
             {1.0, 0.0, 0.0, 0.0},
             {0.0, 1.0, 0.0, 0.0},
@@ -141,8 +122,7 @@ int main(int argc, char **argv){
     torch::Tensor outImg;
 
     for (size_t i = 0; i < iterations; i++){
-        if (device == torch::kCPU){
-            auto p = ProjectGaussiansCPU::apply(means, scales, 1, 
+        auto p = ProjectGaussiansCPU::apply(means, scales, 1, 
                                 quats, viewMat, viewMat,
                                 focal, focal,
                                 width / 2,
@@ -161,32 +141,6 @@ int main(int argc, char **argv){
                 height,
                 width,
                 background);
-        }else{
-            #if defined(USE_HIP) || defined(USE_CUDA) || defined(USE_MPS)
-                auto p = ProjectGaussians::apply(means, scales, 1, 
-                                        quats, viewMat, viewMat,
-                                        focal, focal,
-                                        width / 2,
-                                        height / 2,
-                                        height,
-                                        width,
-                                        tileBounds);
-
-                outImg = RasterizeGaussians::apply(
-                    p[0], // xys
-                    p[1], // depths
-                    p[2], // radii,
-                    p[3], // conics
-                    p[4], // numTilesHit
-                    torch::sigmoid(rgbs),
-                    torch::sigmoid(opacities),
-                    height,
-                    width,
-                    background);
-            #else
-                throw std::runtime_error("GPU support not built, use --cpu");
-            #endif
-        }
 
         outImg.requires_grad_();
         torch::Tensor loss = mseLoss(outImg, gtImage);
