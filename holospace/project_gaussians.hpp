aba531385d454cd27ca9d052ccec580056a23877
gpa21 <gpa21@sfu.ca>
2024-05-19 10:02:43 -0700

    gaussian algo

---
+++ holospace/project_gaussians.hpp
@@ -0,0 +1,48 @@
+#ifndef PROJECT_GAUSSIANS_H
+#define PROJECT_GAUSSIANS_H
+
+#include <torch/torch.h>
+#include "tile_bounds.hpp"
+#include "gsplat.hpp"
+
+using namespace torch::autograd;
+
+class ProjectGaussians : public Function<ProjectGaussians>{
+public:
+    static variable_list forward(AutogradContext *ctx, 
+            torch::Tensor means,
+            torch::Tensor scales,
+            float globScale,
+            torch::Tensor quats,
+            torch::Tensor viewMat,
+            torch::Tensor projMat,
+            float fx,
+            float fy,
+            float cx,
+            float cy,
+            int imgHeight,
+            int imgWidth,
+            TileBounds tileBounds,
+            float clipThresh = 0.01);
+    static tensor_list backward(AutogradContext *ctx, tensor_list grad_outputs);
+};
+
+class ProjectGaussiansCPU{
+public:
+    static variable_list apply( 
+            torch::Tensor means,
+            torch::Tensor scales,
+            float globScale,
+            torch::Tensor quats,
+            torch::Tensor viewMat,
+            torch::Tensor projMat,
+            float fx,
+            float fy,
+            float cx,
+            float cy,
+            int imgHeight,
+            int imgWidth,
+            float clipThresh = 0.01);
+};
+
+#endif
